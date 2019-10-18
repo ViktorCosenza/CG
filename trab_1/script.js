790ffed496ca0206b89f2adf9e0512305a8d64c0
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-10-17 21:04:42 -0300

    seilawq

--- trab_1/script.js
+++ trab_1/script.js
@@ -28,37 +28,31 @@ function main() {
   const sunNode = createPlanet([0.6, 0.6, 0, 1], [0.4, 0.4, 0, 1], 1, 0.005)
   const earthNode = createPlanet([0.2, 0.5, 0.8, 1], [0.8, 0.5, 0.2, 1], 0.5, 0.5);
   const moonNode = createPlanet([0.6, 0.6, 0.6, 1], [0.1, 0.1, 0.1, 1], 0.1, -0.01)
-
-  const mercury = createPlanet([1, 1, 1, 1], [0.5, 0.5, 0.5, 0.5], 0.5, -0.5)
-  const mercuryOrbit = new Node(0.01, [mercury])
   
-  const moonOrbitNode = new Node(0, [moonNode]);
-  const earthOrbitNode = new Node(0.05, [earthNode, moonOrbitNode], 0.5);
-  const solarSystemNode = new Node(0.0, [earthOrbitNode, mercuryOrbit, sunNode]);
+  const moonOrbitNode = new Node({min:1, max:1, speed:0.01}, [moonNode])
+  const earthOrbitNode = new Node({min:2, max:4, speed:0.01}, [earthNode, moonOrbitNode])
+  const solarSystemNode = new Node({min:0, max:0, speed:0.01}, [earthOrbitNode, sunNode])
 
-  earthOrbitNode.localMatrix = m4.translation(100, 0, 0)
-  moonOrbitNode.localMatrix = m4.translation(30, 0, 0)
-  mercuryOrbit.localMatrix = m4.translation(30, 0, 0)
+  twgl.m4.translate(earthOrbitNode.localMatrix, [150, 0, 0], earthOrbitNode.localMatrix)
+  twgl.m4.translate(moonOrbitNode.localMatrix, [30, 0, 0], moonOrbitNode.localMatrix)
 
   var objects = [
     sunNode,
     earthNode,
     moonNode,
-    mercury
   ];
 
   var objectsToDraw = [
     sunNode.drawInfo,
     earthNode.drawInfo,
     moonNode.drawInfo,
-    mercury.drawInfo
   ];
 
     requestAnimationFrame(drawScene);
 
   /* Functions */
   function createPlanet (color, colorMult, scale, rotation, children=[]) {
-    const planet = new Node(rotation, children)
+    const planet = new Node({min:rotation, max:rotation}, children)
     planet.localMatrix = m4.scaling(scale, scale, scale)
     planet.drawInfo = {
       uniforms: {
@@ -82,12 +76,9 @@ function main() {
   function drawScene(time) {
     time *= 0.001;
     resetCanvas(gl)    
-
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     const projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
-
     const viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
-
     /* Tick objects */
     solarSystemNode.tick(true)
     solarSystemNode.updateWorldMatrix();
@@ -117,41 +108,38 @@ uniform vec4 u_colorOffset;
 out vec4 outColor;
 void main() {outColor = v_color * u_colorMult + u_colorOffset;}`
 
-function* interpolateEllipse (min, max) {
-  let n = 0
+function* interpolateEllipse (min, max, speed) {
+  let n = 1
   while (true) {
-    n = n % 1
-    const theta = degToRad(n * 360)
-    const x = Math.cos(theta) * min
-    const y = Math.sin(theta) * max
+    n = n % 2
+    const theta = degToRad((n - 1) * 180)
+    const x = Math.cos(theta) * max
+    const y = Math.sin(theta) * - min
     yield [x, y]
-    n += 0.01
+    n += speed
   }
 }
 
-var Node = function(rotation=0, children, eccentricity=0) {
+var Node = function({max, min, speed=0.01}, children=[]) {
+  this.parent = null
   this.children = children
-  this.eccentricity = eccentricity
-  this.step = 0
-  this.rotation = rotation
+  this.children.map(child => { child.parent = this })
+
   this.localMatrix = m4.identity();
   this.worldMatrix = m4.identity();
-  this.ellipseGenerator = interpolateEllipse(2, 4) 
+  this.max = max
+  this.min = min
+  this.speed = speed
+  this.ellipseGenerator = interpolateEllipse(this.max, this.min, this.speed) 
   this.tick = (recursive=true) => {
-    let dot = twgl.v3.dot(twgl.m4.getAxis(this.localMatrix, 0), [1, 0, 0])
-    let angle = dot / (twgl.v3.length(twgl.m4.getAxis(this.localMatrix, 0))) * 360
-    if (this.eccentricity != 0) {
-      const{value, done} = this.ellipseGenerator.next()
-      const [x,z] = value
-      const nextPos = [x, 0, z]
-      twgl.m4.translate(
-        this.localMatrix,
-        nextPos,
-        this.localMatrix
-      )
-    }
-    else
-      m4.multiply(m4.yRotation(this.rotation), this.localMatrix, this.localMatrix)
+    const{value, done} = this.ellipseGenerator.next()
+    const [x,z] = value
+    let nextPos = [z, 0, x]
+    twgl.m4.translate(
+      this.localMatrix,
+      nextPos,
+      this.localMatrix
+    )
     if (recursive) this.children.map(child => child.tick(recursive))
   }
 }
