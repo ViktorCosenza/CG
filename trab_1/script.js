fd1f36d7812cf27354e2b8e8ac9833a06031081d
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-10-21 15:32:03 -0300

    ok

--- trab_1/script.js
+++ trab_1/script.js
@@ -23,6 +23,7 @@ function main() {
   var fieldOfViewRadians = degToRad(90);
 
   var sphereBufferInfo = flattenedPrimitives.createSphereBufferInfo(gl, 10, 12, 6);
+
   var programInfo = twgl.createProgramInfo(gl, [vs, fs]);
   var sphereVAO = twgl.createVAOFromBufferInfo(gl, programInfo, sphereBufferInfo);
 
@@ -36,12 +37,15 @@ function main() {
     [0.8, 0.5, 0.2, 1], 
     1, 
     0.5);
+  const moonNode = createPlanet(
+    [0.6, 0.6, 0.6, 1], 
+    [0.1, 0.1, 0.1, 1], 
+    0.1, 
+    -0.01)
 
-  const moonNode = createPlanet([0.6, 0.6, 0.6, 1], [0.1, 0.1, 0.1, 1], 0.1, -0.01)
-  const moonOrbitNode = new Node({min:50, max:50, speed:0.01}, [moonNode], [0, 0, 100])
-
+  const moonOrbitNode = new Node({min:50, max:40, speed:0.1}, [moonNode], [0, 0, 100])
   const earthOrbitNode = new Node({min:200, max:150, speed:0.01}, [earthNode, moonOrbitNode], [100, 0, 0])
-  const solarSystemNode = new Node({min:0, max:0, speed:0.01}, [earthOrbitNode, sunNode])
+  const solarSystemNode = new Node({min:0, max:0, speed:0.1}, [earthOrbitNode, sunNode])
 
   var objects = [
     sunNode,
@@ -55,9 +59,33 @@ function main() {
     moonNode.drawInfo,
   ];
 
-    requestAnimationFrame(drawScene);
+  var orbits = earthOrbitNode
+  var orbitProgram = twgl.createProgramInfo(gl, [vertexShaderOrbit, fragmentShaderOrbit])
+  
+  var orbitsToDraw = {
+    active:true,
+    type: gl.LINES,
+    programInfo: orbitProgram,
+    bufferInfo: createOrbit(orbits),
+  }
+  requestAnimationFrame(drawScene);
 
   /* Functions */
+  function createOrbit(node) {
+    const flattened = node.orbit.flat(Infinity)
+    return twgl.createBufferInfoFromArrays(gl, {
+      position: {
+        numComponents: 3,
+         data: flattened.map(el => el/200), 
+      },
+      color: {
+        numComponents: 4,
+        data: new Array(16).fill(1),
+        data: new Array(flattened.length * 4).fill(1)
+      }
+    })
+  }
+  
   function createPlanet (color, colorMult, scale, rotation, children=[]) {
     const planet = new Node({min:rotation, max:rotation}, children)
     planet.localMatrix = m4.scaling([scale, scale, scale])
@@ -76,9 +104,10 @@ function main() {
   function resetCanvas(gl) {
     twgl.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.clearColor(0, 0, 0, 1);
+    gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   }
+
   let then = 0;
   function drawScene(time) {
     time *= 0.01
@@ -96,7 +125,11 @@ function main() {
         object.drawInfo.uniforms.u_matrix = m4.multiply(viewProjectionMatrix, object.worldMatrix);
     });
 
-    twgl.drawObjectList(gl, objectsToDraw);
+    //orbitsToDraw.uniforms.u_matrix = m4.multiply(viewProjectionMatrix, orbitsTo.world)
+    
+    twgl.drawObjectList(gl, objectsToDraw)
+    twgl.drawObjectList(gl, [orbitsToDraw])
+
     requestAnimationFrame(drawScene);
   }
 }
@@ -108,7 +141,29 @@ in vec4 a_position;
 in vec4 a_color;
 uniform mat4 u_matrix;
 out vec4 v_color;
-void main() {gl_Position = u_matrix * a_position;v_color = a_color;}`
+void main() {
+  gl_Position = u_matrix * a_position;
+  v_color = a_color;}`
+
+var vertexShaderOrbit = `#version 300 es
+in vec4 a_position;
+in vec4 a_color;
+out vec4 v_color;
+
+void main() {
+  gl_Position = a_position;
+  v_color = a_color;
+}`
+
+var fragmentShaderOrbit = `#version 300 es
+precision mediump float;
+out vec4 outColor;
+in vec4 v_color;
+
+void main() {
+  outColor = v_color;
+}
+`
 
 var fs = `#version 300 es
 precision mediump float;
@@ -116,26 +171,36 @@ in vec4 v_color;
 uniform vec4 u_colorMult;
 uniform vec4 u_colorOffset;
 out vec4 outColor;
-void main() {outColor = v_color * u_colorMult + u_colorOffset;}`
+void main() {
+  outColor = v_color * u_colorMult + u_colorOffset;}`
+
+function ellipse (min, max, steps=100) {
+  let points = []
+  let n = 0
+  let stepSize = 2/steps
+  while (n <= 2) {
+    const theta = degToRad(n * 180)
+    const x = Math.cos(theta) * max
+    const y = Math.sin(theta) * min
+    points.push([y, x, 0])
+    n += stepSize
+  }
+  return points
+}
 
 function* interpolateEllipse (min, max, speed) {
   let n = 0
   let deltaTime = 0
   while (true) {
     n = n % 2
-    const theta = degToRad((n - 1) * 180)
+    const theta = degToRad(n * 180)
     const x = Math.cos(theta) * max
     const y = Math.sin(theta) * min
     deltaTime = yield [x, y]
     n += speed * deltaTime
   }
 }
-/* TODO:
-  THE INTERPOLATE ELLIPSE FUNCTION SHOULD RETURN ELIPSES THAT HAVE CENTER 0, 0
 
-  THE RETURN FROM INTERPOLATE ELLIPSE IS FIXED BETWEEN A CIRCLE OF CENTER 0,0
-  BUT IN ORDER TO OBJECTS ORBIT ITS PARENT, IT NEEDS TO BE TRANSLATED */
-  
 var Node = function({max, min, speed=0.01}, children=[], translation=[0, 0, 0]) {
   this.parent = null
   this.children = children
@@ -148,6 +213,7 @@ var Node = function({max, min, speed=0.01}, children=[], translation=[0, 0, 0]) 
   this.max = max
   this.min = min
   this.speed = speed
+  this.orbit = ellipse(this.min, this.max)
   this.ellipseGenerator = interpolateEllipse(this.max, this.min, this.speed) 
   this.tick = (recursive=true, deltaTime) => {
     const {value} = this.ellipseGenerator.next(deltaTime)
